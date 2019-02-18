613513898d729b98c3dd9cefc3e959428a7b63be
Chris Williams <syke.dev@gmail.com>
2019-02-18 21:39:21 +0000

    Add script to build and run Pixelmon server locally

---
+++ run-local.sh
@@ -0,0 +1,11 @@
+docker build -t pixelmon-local docker
+
+DATA_DIR=/home/ubuntu/volumes/pixelmon
+MINECRAFT_DIR=/opt/minecraft
+
+docker run --name=pixelmon-container --rm -it -p 25565:25565 \
+-v $DATA_DIR/ops.json:$MINECRAFT_DIR/ops.json \
+-v $DATA_DIR/server.properties:$MINECRAFT_DIR/server.properties \
+-v $DATA_DIR/config:$MINECRAFT_DIR/config \
+-v $DATA_DIR/world:$MINECRAFT_DIR/world \
+pixelmon-local java -jar minecraft-forge.jar nogui
